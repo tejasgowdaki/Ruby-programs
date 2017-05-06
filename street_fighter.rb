0ac8cc6d9d4fa7cba6744a0177c7a01a9d77820f
tejasgowdaki <tejasgowda80@gmail.com>
2017-05-06 15:23:02 +0530

    small change

--- street_fighter.rb
+++ street_fighter.rb
@@ -74,42 +74,38 @@ Result:
 class StreetFighter
 
 	FIGHTERS = [
-			    	["Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"],
-			    	["Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"]
+			    	['Ryu', 'E.Honda', 'Blanka', 'Guile', 'Balrog', 'Vega'],
+			    	['Ken', 'Chun Li', 'Zangief', 'Dhalsim', 'Sagat', 'M.Bison']
 				]
 
 	@@x = 0
 	@@y = 0
-	
+
 	def self.move_position(move)
 		case move
-		when "right"
+		when 'right'
 			(@@y == 5)? @@y = 0 : @@y += 1
-		when "left"
+		when 'left'
 			(@@y == 0)? @@y = 5 : @@y -= 1
-		when "up"				
+		when 'up'
 			@@x = 0
-		when "down"
+		when 'down'
 			@@x = 1
-		else 
-			return "Invalid entry"
+		else
+			return 'Invalid entry'
 		end
-		return FIGHTERS[@@x][@@y]
+		FIGHTERS[@@x][@@y]
 	end
-	
-	
 end
 
-puts "Enter the move"
+puts 'Enter the move'
 puts "Enter up/down/right/left\nEnter exit to exit program"
 
 move = gets.chomp
 
-while move != "exit"
-
-	current_player = StreetFighter.move_position(move)
-	puts "Player - #{current_player}"
-
-	puts "Enter the move"
-	move = gets.chomp
+while move != 'exit'
+	StreetFighter.move_position(move)
+puts "Player - #{current_player}"
+puts 'Enter the move'
+move = gets.chomp
 end
